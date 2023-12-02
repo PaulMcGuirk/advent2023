5bb2144e82167f8f3866ab43b7249ddcbcdae4a0
paul <paul@fulcrumpro.com>
2023-12-02 08:03:53 -0600

    Simplify (?) solution

--- rust/day02/src/main.rs
+++ rust/day02/src/main.rs
@@ -3,72 +3,47 @@ use std::time::Instant;
 
 const FILEPATH: &str = "./input/input.txt";
 
-struct CubeSet { red: u32, green: u32, blue: u32}
+const NUM_COLORS: usize = 3;
+const COLORS: &[&str; NUM_COLORS] = &["red", "green", "blue"];
 
-impl CubeSet {
-    fn parse(s: &str) -> Self {
-        let mut red = 0;
-        let mut green = 0;
-        let mut blue = 0;
-
-        for pc in s.split(',') {
-            let mut sub_pcs = pc.split_whitespace();
-            let qty = sub_pcs.next().unwrap().trim().parse::<u32>().unwrap();
-            let color = sub_pcs.next().unwrap().trim();
-
-            match color {
-                "red" => red = qty,
-                "green" => green = qty,
-                "blue" => blue = qty,
-                _ => panic!()
+fn parse_input(s: &str) -> Vec<Vec<Vec<u32>>> {
+    s.trim().lines().map(|line| {
+        let draws = line.trim().split(":").skip(1).next().unwrap();
+        draws.split(";").map(|d| {
+            let mut cubes = vec![0, 0, 0];
+            for pc in d.trim().split(",") {
+                let mut sub_pcs = pc.split_whitespace();
+                let qty = sub_pcs.next().unwrap().trim().parse::<u32>().unwrap();
+                let color = sub_pcs.next().unwrap().trim();
+                let idx = COLORS.iter().enumerate().find(|(_, &c)| c == color).unwrap().0;
+                cubes[idx] = qty;
             }
-        }
-
-        Self { red, green, blue }
-    }
+            cubes
+        }).collect::<Vec<_>>()
+    }).collect::<Vec<_>>()
 }
 
-struct Game {
-    id: u32,
-    draws: Vec<CubeSet>
+fn possible_games(games: &Vec<Vec<Vec<u32>>>, cube_set: &Vec<u32>) -> u32 {
+    games.iter().enumerate()
+        .filter_map(|(idx, draws)| {
+            let possible = draws.iter().all(|d| {
+                (0..NUM_COLORS).all(|i| d[i] <= cube_set[i])
+            });
+
+            if possible {
+                Some(idx as u32 + 1)
+            } else {
+                None
+            }
+    }).sum()
 }
 
-impl Game {
-    fn parse(s: &str) -> Self {
-        let mut pcs = s.split(":");
-
-        let id = pcs.next().unwrap().split_whitespace().skip(1).last().unwrap().parse::<u32>().unwrap();
-
-        let draws = pcs.next().unwrap().split(";").map(|d| CubeSet::parse(d.trim())).collect::<Vec<_>>();
-
-        Self { id, draws }
-    }
-
-    fn possible(&self, cube_set: &CubeSet) -> bool {
-        self.draws.iter().all(|d| d.red <= cube_set.red && d.green <= cube_set.green && d.blue <= cube_set.blue)
-    }
-
-    fn power(&self) -> u32 {
-        let min_red = self.draws.iter().map(|d| d.red).max().unwrap();
-        let min_green = self.draws.iter().map(|d| d.green).max().unwrap();
-        let min_blue = self.draws.iter().map(|d| d.blue).max().unwrap();
-
-        min_red * min_green * min_blue
-    }
-}
-
-fn parse_input(input: &str) -> Vec<Game> {
-    input.trim().lines()
-        .map(|line| Game::parse(line.trim()))
-        .collect::<Vec<_>>()
-}
-
-fn possible_games(games: &Vec<Game>, cube_set: &CubeSet) -> u32 {
-    games.iter().filter_map(|g| if g.possible(cube_set) { Some(g.id) } else { None }).sum()
-}
-
-fn total_power(games: &Vec<Game>) -> u32 {
-    games.iter().map(|g| g.power()).sum()
+fn total_power(games: &Vec<Vec<Vec<u32>>>) -> u32 {
+    games.iter().map(|draws| (0..NUM_COLORS)
+        .map(|i| draws.iter()
+            .map(|draw| draw[i]).max().unwrap()
+        ).product::<u32>()
+    ).sum()
 }
 
 fn main() {
@@ -80,7 +55,7 @@ fn main() {
     let input = fs::read_to_string(FILEPATH).expect("Could not read file");
     let games = parse_input(&input);
 
-    let part_one = possible_games(&games, &CubeSet { red: 12, green: 13, blue: 14 });
+    let part_one = possible_games(&games, &vec![12, 13, 14]);
     let part_two = total_power(&games);
 
     println!("Part one: {}", part_one);
