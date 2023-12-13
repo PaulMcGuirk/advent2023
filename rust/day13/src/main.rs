0c1edd63e39b2b54fb2d81852820f67fa594baa7
paul <paul@fulcrumpro.com>
2023-12-13 06:01:47 -0600

    Clean up rust solution for day 13

--- rust/day13/src/main.rs
+++ rust/day13/src/main.rs
@@ -3,97 +3,50 @@ use std::time::Instant;
 
 const FILEPATH: &str = "./input/input.txt";
 
-fn part_one(grids: &Vec<Vec<Vec<bool>>>) -> usize {
-    grids.iter().map(|grid| summarize(grid)).sum()
+fn summarize_grids(grids: &Vec<Vec<Vec<bool>>>, smudge: bool) -> usize {
+    grids.iter().map(|grid| summarize_grid(grid, smudge)).sum()
 }
 
-fn part_two(grids: &Vec<Vec<Vec<bool>>>) -> usize {
-    grids.iter().map(|grid| summarize_with_smudge(grid)).sum()
-}
-
-fn summarize(grid: &Vec<Vec<bool>>) -> usize {
+fn summarize_grid(grid: &Vec<Vec<bool>>, smudge: bool) -> usize {
     let num_rows = grid.len();
     let num_cols = grid[0].len();
 
-    let col_nums = (0..num_cols)
-        .map(|c| (0..num_rows).fold(0u32, |acc, r| (acc << 1) + if grid[r][c] { 1 } else { 0 }))
-        .collect::<Vec<_>>();
-    let row_nums = (0..num_rows)
-        .map(|r| (0..num_cols).fold(0u32, |acc, c| (acc << 1) + if grid[r][c] { 1 } else { 0 }))
-        .collect::<Vec<_>>();
-
-    let col_rel = find_mirror(&col_nums);
-
-    if let Some(col_rel) = col_rel {
-        col_rel + 1
+    if let Some(val) = find_mirror(grid, smudge) {
+        100 * (val + 1)
     } else {
-        let row_rel = find_mirror(&row_nums).unwrap();
-        100 * (row_rel + 1)
+        let tranpose = (0..num_cols)
+            .map(|c| (0..num_rows).map(|r| grid[r][c]).collect::<Vec<_>>())
+            .collect::<Vec<_>>();
+        let val = find_mirror(&tranpose, smudge).unwrap();
+        val + 1
     }
 }
 
-fn summarize_with_smudge(grid: &Vec<Vec<bool>>) -> usize {
-    let num_rows = grid.len();
-    let num_cols = grid[0].len();
-
-    let tranpose = (0..num_cols)
-        .map(|c| (0..num_rows).map(|r| grid[r][c]).collect::<Vec<_>>())
-        .collect::<Vec<_>>();
-
-    if let Some(val) = find_mirror_with_smudge(&tranpose) {
-        return val + 1;
-    }
-
-    let val = find_mirror_with_smudge(&grid).unwrap();
-    100 * (val + 1)
-}
-
-fn find_mirror(nums: &Vec<u32>) -> Option<usize> {
-    let max = nums.len();
-    (0..(max - 1)).find(|&i| {
-        (0..=i).all(|j| {
-            let k = i + j + 1;
-            k >= max || nums[k] == nums[i - j]
-        })
-    })
-}
-
-fn find_mirror_with_smudge(matrix: &Vec<Vec<bool>>) -> Option<usize> {
+fn find_mirror(matrix: &Vec<Vec<bool>>, smudge: bool) -> Option<usize> {
     let num_rows = matrix.len();
     let num_cols = matrix[0].len();
 
-    for i in 0..(num_rows - 1) {
-        let mut repl = false;
-        for j in 0..=i {
-            let v1 = &matrix[i - j];
-            if i + j + 1 >= num_rows {
-                break;
-            }
-            let v2 = &matrix[i + j + 1];
-            let diffs = (0..num_cols)
-                .filter(|&k| v1[k] != v2[k])
-                .collect::<Vec<_>>();
-            if diffs.is_empty() {
-                continue;
-            }
-            if diffs.len() > 1 {
-                repl = false;
-                break;
-            }
-            if repl {
-                repl = false;
-                break; // more than 1 diff
-            }
-
-            repl = true;
+    (0..(num_rows) - 1).find(|&i| {
+        let max = (i + 1).min(num_rows - i - 1);
+        let (single_error_count, multiple_error_count) =
+            (0..max).fold((0, 0), |(sing, mult), j| {
+                let v1 = &matrix[i - j];
+                let v2 = &matrix[i + j + 1];
+                let errors = (0..num_cols).filter(|&k| v1[k] != v2[k]).count();
+                if errors == 0 {
+                    (sing, mult)
+                } else if errors == 1 {
+                    (sing + 1, mult)
+                } else {
+                    (sing, mult + 1)
+                }
+            });
+        if smudge {
+            multiple_error_count == 0 && single_error_count == 1
+        } else {
+            multiple_error_count == 0 && single_error_count == 0
         }
-
-        if repl {
-            return Some(i);
-        }
-    }
-
-    None
+    })
 }
 
 fn parse_input(input: &str) -> Vec<Vec<Vec<bool>>> {
@@ -115,26 +68,11 @@ fn main() {
 
     let now = Instant::now();
 
-    let raw_input = "#.##..##.
-..#.##.#.
-##......#
-##......#
-..#.##.#.
-..##..##.
-#.#.##.#.
-
-#...##..#
-#....#..#
-..##..###
-#####.##.
-#####.##.
-..##..###
-#....#..#";
     let raw_input = fs::read_to_string(FILEPATH).expect("Could not read file");
     let grids = parse_input(&raw_input);
 
-    let part_one = part_one(&grids);
-    let part_two = part_two(&grids);
+    let part_one = summarize_grids(&grids, false);
+    let part_two = summarize_grids(&grids, true);
 
     println!("Part one: {}", part_one);
     println!("Part two: {}", part_two);
