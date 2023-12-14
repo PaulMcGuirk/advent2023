1bb2cc845b90445d4210cffae1f9dd9b094d6546
paul <paul@fulcrumpro.com>
2023-12-14 08:07:00 -0600

    Clean up day 14

--- rust/day14/src/main.rs
+++ rust/day14/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 use std::fs;
 use std::time::Instant;
 
@@ -11,163 +11,95 @@ enum Tile {
     Empty,
 }
 
-fn print_grid(grid: &Vec<Vec<Tile>>) {
-    for row in grid.iter() {
-        for c in row.iter() {
-            let ch = match c {
-                Tile::Round => 'O',
-                Tile::Cube => '█',
-                Tile::Empty => ' ',
+enum TiltDirection {
+    North,
+    South,
+    East,
+    West,
+}
+
+fn tilt(grid: &Vec<Vec<Tile>>, dir: TiltDirection) -> Vec<Vec<Tile>> {
+    let size = grid.len();
+
+    let mut grid = grid.clone();
+
+    for a in 0..size {
+        for b in 0..size {
+            let (r, c) = match dir {
+                TiltDirection::North => (b, a),
+                TiltDirection::West => (a, b),
+                TiltDirection::South => (size - 1 - b, a),
+                TiltDirection::East => (a, size - 1 - b),
             };
-            print!("{}", ch);
+            if grid[r][c] != Tile::Round {
+                continue;
+            }
+
+            let (mut s_r, mut s_c) = (r as isize, c as isize);
+            loop {
+                let (n_r, n_c) = match dir {
+                    TiltDirection::North => (s_r - 1, s_c),
+                    TiltDirection::West => (s_r, s_c - 1),
+                    TiltDirection::South => (s_r + 1, s_c),
+                    TiltDirection::East => (s_r, s_c + 1),
+                };
+                if n_r < 0 || n_r >= size as isize || n_c < 0 || n_c >= size as isize {
+                    break;
+                }
+                if grid[n_r as usize][n_c as usize] != Tile::Empty {
+                    break;
+                }
+
+                (s_r, s_c) = (n_r, n_c);
+            }
+
+            grid[r][c] = Tile::Empty;
+            grid[s_r as usize][s_c as usize] = Tile::Round;
         }
-        println!();
     }
-    println!();
+
+    grid
 }
 
-fn tilt_north(grid: &Vec<Vec<Tile>>) -> Vec<Vec<Tile>> {
-    let num_rows = grid.len();
-    let num_cols = grid[0].len();
-
-    let mut new_grid = grid.clone();
-
-    for c in 0..num_cols {
-        let mut r = 0;
-        while r < num_rows {
-            if grid[r][c] == Tile::Empty {
-                let mut s_r = r;
-                loop {
-                    r += 1;
-                    if r >= num_rows {
-                        break;
-                    }
-                    match grid[r][c] {
-                        Tile::Cube => {
-                            break;
-                        }
-                        Tile::Empty => {}
-                        Tile::Round => {
-                            new_grid[r][c] = Tile::Empty;
-                            new_grid[s_r][c] = Tile::Round;
-                            s_r += 1;
-                        }
-                    }
-                }
-            }
-            r += 1;
-        }
-    }
-
-    new_grid
+fn get_key(grid: &Vec<Vec<Tile>>) -> String {
+    grid.iter()
+        .flat_map(|row| {
+            row.iter().map(|r| match r {
+                Tile::Round => 'O',
+                Tile::Empty => '.',
+                Tile::Cube => '#',
+            })
+        })
+        .collect::<String>()
 }
 
-fn tilt_south(grid: &Vec<Vec<Tile>>) -> Vec<Vec<Tile>> {
-    let num_rows = grid.len();
-    let num_cols = grid[0].len();
+fn spin_cycle(grid: &Vec<Vec<Tile>>, cycle_count: usize) -> usize {
+    let mut grid = grid.clone();
+    let s = get_key(&grid);
 
-    let mut new_grid = grid.clone();
+    let mut seen = HashMap::new();
+    let mut history = vec![];
+    history.push(get_pressure(&grid));
+    seen.insert(s, 0);
 
-    for c in 0..num_cols {
-        let mut r = num_rows as isize - 1;
-        while r >= 0 {
-            if grid[r as usize][c] == Tile::Empty {
-                let mut s_r = r;
-                loop {
-                    r -= 1;
-                    if r < 0 {
-                        break;
-                    }
-                    match grid[r as usize][c] {
-                        Tile::Cube => {
-                            break;
-                        }
-                        Tile::Empty => {}
-                        Tile::Round => {
-                            new_grid[r as usize][c] = Tile::Empty;
-                            new_grid[s_r as usize][c] = Tile::Round;
-                            s_r -= 1;
-                        }
-                    }
-                }
-            }
-            r -= 1;
+    for i in 1..=cycle_count {
+        grid = tilt(&grid, TiltDirection::North);
+        grid = tilt(&grid, TiltDirection::West);
+        grid = tilt(&grid, TiltDirection::South);
+        grid = tilt(&grid, TiltDirection::East);
+
+        let s = get_key(&grid);
+
+        if let Some(&prev_seen) = seen.get(&s) {
+            let period = i - prev_seen;
+            return history[prev_seen + (cycle_count - prev_seen) % period];
         }
+
+        seen.insert(s, i);
+        history.push(get_pressure(&grid));
     }
 
-    new_grid
-}
-
-fn tilt_west(grid: &Vec<Vec<Tile>>) -> Vec<Vec<Tile>> {
-    let num_rows = grid.len();
-    let num_cols = grid[0].len();
-
-    let mut new_grid = grid.clone();
-
-    for r in 0..num_rows {
-        let mut c = 0;
-        while c < num_rows {
-            if grid[r][c] == Tile::Empty {
-                let mut s_c = c;
-                loop {
-                    c += 1;
-                    if c >= num_cols {
-                        break;
-                    }
-                    match grid[r][c] {
-                        Tile::Cube => {
-                            break;
-                        }
-                        Tile::Empty => {}
-                        Tile::Round => {
-                            new_grid[r][c] = Tile::Empty;
-                            new_grid[r][s_c] = Tile::Round;
-                            s_c += 1;
-                        }
-                    }
-                }
-            }
-            c += 1;
-        }
-    }
-
-    new_grid
-}
-
-fn tilt_east(grid: &Vec<Vec<Tile>>) -> Vec<Vec<Tile>> {
-    let num_rows = grid.len();
-    let num_cols = grid[0].len();
-
-    let mut new_grid = grid.clone();
-
-    for r in 0..num_rows {
-        let mut c = num_cols as isize - 1;
-        while c >= 0 {
-            if grid[r][c as usize] == Tile::Empty {
-                let mut s_c = c;
-                loop {
-                    c -= 1;
-                    if c < 0 {
-                        break;
-                    }
-                    match grid[r][c as usize] {
-                        Tile::Cube => {
-                            break;
-                        }
-                        Tile::Empty => {}
-                        Tile::Round => {
-                            new_grid[r][c as usize] = Tile::Empty;
-                            new_grid[r][s_c as usize] = Tile::Round;
-                            s_c -= 1;
-                        }
-                    }
-                }
-            }
-            c -= 1;
-        }
-    }
-
-    new_grid
+    panic!("Not found");
 }
 
 fn get_pressure(grid: &Vec<Vec<Tile>>) -> usize {
@@ -196,102 +128,20 @@ fn parse_input(input: &str) -> Vec<Vec<Tile>> {
         .collect::<Vec<_>>()
 }
 
-fn cycle(grid: &Vec<Vec<Tile>>) -> Vec<Vec<Tile>> {
-    // todo: we could probably do this with rotating and tilting the rotations
-    let grid = tilt_north(&grid);
-    let grid = tilt_west(&grid);
-    let grid = tilt_south(&grid);
-    let grid = tilt_east(&grid);
-
-    // let grid = rotate(&grid);
-    // let grid = tilt_north(&grid);
-
-    // let grid = rotate(&grid);
-    // let grid = tilt_north(&grid);
-
-    // let grid = rotate(&grid);
-    // let grid = tilt_north(&grid);
-
-    grid
-}
-
-fn solve_part_two(grid: &Vec<Vec<Tile>>, num_cylces: usize) -> usize {
-    let mut pressures = HashMap::new();
-    let pressure = get_pressure(&grid);
-
-    pressures.insert(pressure, 0);
-
-    let mut grid = grid.clone();
-
-    let mut hist = vec![];
-    hist.push(pressure);
-
-    for i in 0..200 {
-        grid = cycle(&grid);
-        let p = get_pressure(&grid);
-        println!("{} {}", i + 1, p);
-        // hist.push(p);
-
-        // if pressures.contains_key(&p) {
-        //     panic!("rep! {}", i + 1);
-        // }
-
-        // pressures.insert(p, i + 1);
-    }
-
-    println!("{:?}", hist);
-
-    0
-}
-
-// fn tranpose(grid: &Vec<Vec<Tile>>) -> Vec<Vec<Tile>> {
-//     let num_rows = grid.len();
-//     let num_cols = grid[0].len();
-
-//     let mut trans = vec![vec![Tile::Empty; num_rows]; num_cols];
-//     for r in 0..num_rows {
-//         for c in 0..num_cols {
-//             trans[c][r] = grid[r][c]
-//         }
-//     }
-
-//     trans
-// }
-
 fn main() {
     println!("Advent of Code 2023");
     println!("Day 14: Parabolic Reflector Dish");
 
     let now = Instant::now();
 
-    let raw_input = "O....#....
-O.OO#....#
-.....##...
-OO.#O....O
-.O.....O#.
-O.#..O.#.#
-..O..#O..O
-.......O..
-#....###..
-#OO..#....";
     let raw_input = fs::read_to_string(FILEPATH).expect("Could not read file");
     let platform = parse_input(&raw_input);
-    let tilted = tilt_north(&platform);
-    let part_one = get_pressure(&tilted);
 
-    let part_two = solve_part_two(&platform, 1000000000);
-    // print_grid(&tilted);
-
-    // let cycled = cycle(&platform);
-    // print_grid(&cycled);
-    // let cycled = cycle(&platform);
-    // print_grid(&cycled);
-
-    // let part_one = summarize_grids(&grids, false);
-    // let part_two = summarize_grids(&grids, true);
+    let part_one = get_pressure(&tilt(&platform, TiltDirection::North));
+    let part_two = spin_cycle(&platform, 1000000000);
 
     println!("Part one: {}", part_one);
-    println!("Part two: review output manuall");
+    println!("Part two: {}", part_two);
 
     println!("Elasped time: {}ms", now.elapsed().as_millis());
 }
