60288086ffc34554d262a29122223ddec55fe4e3
paul <paul@fulcrumpro.com>
2023-12-22 08:41:49 -0600

    Clean up rust solution for day 22

--- rust/day22/src/main.rs
+++ rust/day22/src/main.rs
@@ -1,160 +1,101 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 use std::fs;
 use std::time::Instant;
 
 const FILEPATH: &str = "./input/input.txt";
 
-fn count_safe(bricks: &Vec<Vec<Vec<usize>>>) -> usize {
-    let mut bricks = bricks.clone();
-    bricks.sort_by_key(|b| b.iter().map(|c| c[2]).min().unwrap());
+#[derive(Copy, Clone)]
+struct Brick {
+    min_x: usize,
+    min_y: usize,
+    min_z: usize,
+    max_x: usize,
+    max_y: usize,
+    max_z: usize,
+}
 
-    let maxes = (0..3)
+fn solve(bricks: &Vec<Brick>) -> (usize, usize) {
+    let bricks = {
+        let mut bricks = bricks.clone();
+        bricks.sort_by_key(|b| b.min_z);
+        bricks
+    };
+
+    let (baseline, supported_by) = resolve_falls(&bricks, None);
+
+    let safe_count = (0..bricks.len())
+        .filter(|i| !supported_by.iter().any(|s| s.len() == 1 && s.contains(i)))
+        .count();
+
+    let total_falls = (0..bricks.len())
         .map(|i| {
-            bricks
-                .iter()
-                .map(|b| b.iter().map(|c| c[i]).max().unwrap())
-                .max()
-                .unwrap()
-                + 1
+            let (fallen, _) = resolve_falls(&bricks, Some(i));
+            (0..bricks.len())
+                .filter(|&j| j != i && baseline[j].min_z > fallen[j].min_z)
+                .count()
         })
-        .collect::<Vec<_>>();
+        .sum();
 
-    let mut tiles = vec![vec![vec![Option::<usize>::None; maxes[2]]; maxes[1]]; maxes[0]];
-    let mut supports = vec![vec![]; bricks.len()];
+    (safe_count, total_falls)
+}
 
-    for (i, brick) in bricks.iter_mut().enumerate() {
-        let x_min = brick.iter().map(|c| c[0]).min().unwrap();
-        let x_max = brick.iter().map(|c| c[0]).max().unwrap();
-        let y_min = brick.iter().map(|c| c[1]).min().unwrap();
-        let y_max = brick.iter().map(|c| c[1]).max().unwrap();
-        let z_min = brick.iter().map(|c| c[2]).min().unwrap();
+fn resolve_falls(bricks: &Vec<Brick>, omit: Option<usize>) -> (Vec<Brick>, Vec<HashSet<usize>>) {
+    let x_bound = bricks.iter().map(|b| b.max_x).max().unwrap() + 1;
+    let y_bound = bricks.iter().map(|b| b.max_y).max().unwrap() + 1;
+    let z_bound = bricks.iter().map(|b| b.max_z).max().unwrap() + 1;
 
-        let z = (1usize..z_min)
+    let mut tiles = vec![vec![vec![Option::<usize>::None; z_bound]; y_bound]; x_bound];
+    let mut supported_by = vec![];
+    let mut fallen = vec![];
+
+    for (i, brick) in bricks.iter().enumerate() {
+        if Some(i) == omit {
+            supported_by.push(HashSet::new());
+            fallen.push(brick.clone());
+            continue;
+        }
+        let z = (1usize..brick.min_z)
             .rev()
-            .find(|&z| (x_min..=x_max).any(|x| (y_min..=y_max).any(|y| tiles[x][y][z].is_some())))
+            .find(|&z| {
+                (brick.min_x..=brick.max_x)
+                    .any(|x| (brick.min_y..=brick.max_y).any(|y| tiles[x][y][z].is_some()))
+            })
             .unwrap_or(0)
             + 1;
 
-        let below = (x_min..=x_max)
+        let below = (brick.min_x..=brick.max_x)
             .flat_map(|x| {
-                (y_min..=y_max)
+                (brick.min_y..=brick.max_y)
                     .filter_map(|y| tiles[x][y][z - 1])
                     .collect::<HashSet<_>>()
             })
             .collect::<HashSet<_>>();
-        supports[i].extend(below);
 
-        for c in brick.iter_mut() {
-            c[2] = z + c[2] - z_min;
-            tiles[c[0]][c[1]][c[2]] = Some(i);
+        supported_by.push(below);
+        let fallen_brick = Brick {
+            min_x: brick.min_x,
+            min_y: brick.min_y,
+            min_z: z,
+            max_x: brick.max_x,
+            max_y: brick.max_y,
+            max_z: z + brick.max_z - brick.min_z,
+        };
+
+        for x in fallen_brick.min_x..=fallen_brick.max_x {
+            for y in fallen_brick.min_y..=fallen_brick.max_y {
+                for z in fallen_brick.min_z..=fallen_brick.max_z {
+                    tiles[x][y][z] = Some(i);
+                }
+            }
         }
+
+        fallen.push(fallen_brick);
     }
 
-    (0..bricks.len())
-        .filter(|&i| !supports.iter().any(|s| s.len() == 1 && s[0] == i))
-        .count()
+    (fallen, supported_by)
 }
 
-fn chain_reaction(bricks: &Vec<Vec<Vec<usize>>>) -> usize {
-    let mut bricks = bricks.clone();
-    bricks.sort_by_key(|b| b.iter().map(|c| c[2]).min().unwrap());
-
-    let baseline = {
-        let mut bricks = bricks.clone();
-
-        let maxes = (0..3)
-            .map(|i| {
-                bricks
-                    .iter()
-                    .map(|b| b.iter().map(|c| c[i]).max().unwrap())
-                    .max()
-                    .unwrap()
-                    + 1
-            })
-            .collect::<Vec<_>>();
-
-        let mut tiles = vec![vec![vec![Option::<usize>::None; maxes[2]]; maxes[1]]; maxes[0]];
-
-        for (i, brick) in bricks.iter_mut().enumerate() {
-            let x_min = brick.iter().map(|c| c[0]).min().unwrap();
-            let x_max = brick.iter().map(|c| c[0]).max().unwrap();
-            let y_min = brick.iter().map(|c| c[1]).min().unwrap();
-            let y_max = brick.iter().map(|c| c[1]).max().unwrap();
-            let z_min = brick.iter().map(|c| c[2]).min().unwrap();
-
-            let z = (1usize..z_min)
-                .rev()
-                .find(|&z| {
-                    (x_min..=x_max).any(|x| (y_min..=y_max).any(|y| tiles[x][y][z].is_some()))
-                })
-                .unwrap_or(0)
-                + 1;
-
-            for c in brick.iter_mut() {
-                c[2] = z + c[2] - z_min;
-                tiles[c[0]][c[1]][c[2]] = Some(i);
-            }
-        }
-
-        bricks
-    };
-
-    let mut res = 0;
-    for dis in 0..bricks.len() {
-        let mut bricks = bricks.clone();
-
-        bricks.sort_by_key(|b| b.iter().map(|c| c[2]).min().unwrap());
-
-        let maxes = (0..3)
-            .map(|i| {
-                bricks
-                    .iter()
-                    .map(|b| b.iter().map(|c| c[i]).max().unwrap())
-                    .max()
-                    .unwrap()
-                    + 1
-            })
-            .collect::<Vec<_>>();
-
-        let mut tiles = vec![vec![vec![Option::<usize>::None; maxes[2]]; maxes[1]]; maxes[0]];
-
-        for (i, brick) in bricks.iter_mut().enumerate() {
-            if i == dis {
-                continue;
-            }
-            let x_min = brick.iter().map(|c| c[0]).min().unwrap();
-            let x_max = brick.iter().map(|c| c[0]).max().unwrap();
-            let y_min = brick.iter().map(|c| c[1]).min().unwrap();
-            let y_max = brick.iter().map(|c| c[1]).max().unwrap();
-            let z_min = brick.iter().map(|c| c[2]).min().unwrap();
-
-            let z = (1usize..z_min)
-                .rev()
-                .find(|&z| {
-                    (x_min..=x_max).any(|x| (y_min..=y_max).any(|y| tiles[x][y][z].is_some()))
-                })
-                .unwrap_or(0)
-                + 1;
-
-            for c in brick.iter_mut() {
-                c[2] = z + c[2] - z_min;
-                tiles[c[0]][c[1]][c[2]] = Some(i);
-            }
-        }
-
-        let fell = (0..bricks.len())
-            .filter(|&i| {
-                i != dis
-                    && baseline[i].iter().map(|c| c[2]).min() > bricks[i].iter().map(|c| c[2]).min()
-            })
-            .count();
-        res += fell;
-    }
-
-    res
-}
-
-fn parse_input(input: &str) -> Vec<Vec<Vec<usize>>> {
+fn parse_input(input: &str) -> Vec<Brick> {
     input
         .trim()
         .lines()
@@ -168,16 +109,21 @@ fn parse_input(input: &str) -> Vec<Vec<Vec<usize>>> {
                         .collect::<Vec<_>>()
                 })
                 .collect::<Vec<_>>();
-            let diff = (0..3).find(|&i| ends[0][i] != ends[1][i]).unwrap_or(0);
-            let start = ends[0][diff].min(ends[1][diff]);
-            let end = ends[0][diff].max(ends[1][diff]);
-            (start..=end)
-                .map(|d| {
-                    (0..3)
-                        .map(|i| if i == diff { d } else { ends[0][i] })
-                        .collect::<Vec<_>>()
-                })
-                .collect::<Vec<_>>()
+            let min = (0..3)
+                .map(|i| ends.iter().map(|end| end[i]).min().unwrap())
+                .collect::<Vec<_>>();
+            let max = (0..3)
+                .map(|i| ends.iter().map(|end| end[i]).max().unwrap())
+                .collect::<Vec<_>>();
+
+            Brick {
+                min_x: min[0],
+                min_y: min[1],
+                min_z: min[2],
+                max_x: max[0],
+                max_y: max[1],
+                max_z: max[2],
+            }
         })
         .collect::<Vec<_>>()
 }
@@ -188,21 +134,10 @@ fn main() {
 
     let now = Instant::now();
 
-    let raw_input = "1,0,1~1,2,1
-0,0,2~2,0,2
-0,2,3~2,2,3
-0,0,4~0,2,4
-2,0,5~2,2,5
-0,1,6~2,1,6
-1,1,8~1,1,9
-";
-
     let raw_input = fs::read_to_string(FILEPATH).expect("Could not read file");
-
     let bricks = parse_input(&raw_input);
 
-    let part_one = count_safe(&bricks);
-    let part_two = chain_reaction(&bricks);
+    let (part_one, part_two) = solve(&bricks);
 
     println!("Part one: {}", part_one);
     println!("Part two: {}", part_two);
