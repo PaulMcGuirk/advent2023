a541255a158283fc1e5d92f80194d6df722c8c07
paul <paul@fulcrumpro.com>
2023-12-22 08:02:11 -0600

    Clean up rust solution for day 21

--- rust/day21/src/main.rs
+++ rust/day21/src/main.rs
@@ -1,49 +1,10 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 use std::fs;
 use std::time::Instant;
 
 const FILEPATH: &str = "./input/input.txt";
 
-// fn navigate_big_dump_two(
-//     start: (i32, i32),
-//     rocks: &HashSet<(i32, i32)>,
-//     num_steps: usize,
-// ) -> usize {
-//     let ds = vec![(1, 0), (-1, 0), (0, 1), (0, -1)];
-//     let mut reached = HashSet::new();
-//     reached.insert(start);
-
-//     for i in 1..=num_steps {
-//         let mut next = HashSet::new();
-
-//         for (r, c) in reached {
-//             for (d_r, d_c) in ds.iter() {
-//                 let n = (r + d_r, c + d_c);
-//                 if !rocks.contains(&n) {
-//                     next.insert(n);
-//                 }
-//             }
-//         }
-
-//         reached = next;
-
-//         let val = reached.next();
-
-//         if i > 65 && (i - 65) % 131 == 0 && ((i - 65) / 131) % 2 == 0 {
-//             println!("{}, {}", (i - 65) / 131, res);
-//         }
-//     }
-
-//     reached.len()
-// }
-
-fn navigate_big(
-    num_rows: i32,
-    num_cols: i32,
-    start: (i32, i32),
-    rocks: &HashSet<(i32, i32)>,
-    num_steps: usize,
-) -> usize {
+fn navigate(start: (i32, i32), rocks: &HashSet<(i32, i32)>, num_steps: usize) -> usize {
     let ds = vec![(1, 0), (-1, 0), (0, 1), (0, -1)];
 
     let mut prev = HashSet::<(i32, i32)>::new();
@@ -58,9 +19,7 @@ fn navigate_big(
         for (r, c) in curr.iter() {
             for (d_r, d_c) in ds.iter() {
                 let (n_r, n_c) = (r + d_r, c + d_c);
-                if !rocks.contains(&(n_r.rem_euclid(num_rows), n_c.rem_euclid(num_cols)))
-                    && !prev.contains(&(n_r, n_c))
-                {
+                if !rocks.contains(&(n_r, n_c)) && !prev.contains(&(n_r, n_c)) {
                     next.insert((n_r, n_c));
                 }
             }
@@ -77,57 +36,91 @@ fn navigate_big(
     res
 }
 
-fn navigate_big_two(
+fn navigate_infinite(
     num_rows: i32,
     num_cols: i32,
     start: (i32, i32),
     rocks: &HashSet<(i32, i32)>,
-    num_steps: usize,
-) -> u64 {
+    num_steps: i32,
+) -> i64 {
+    // this solution isn't at all generic. it relies on the fact that the input is square, that
+    // we start in the middle, and the that number of steps is such that we'll end on complete
+    // squares going to the left, right, up, and down from the initial square
+    // under these conditions, the solution is one of two quadratic function in the number of complete
+    // squares traversed, where which quadratic function used depends on the parity of the number of complete
+    // squares traversed.
+    // none of these observations are mine - all stolen from reddit especially
+    // https://www.reddit.com/r/adventofcode/comments/18nol3m/2023_day_21_a_geometric_solutionexplanation_for/
+
+    assert_eq!(num_rows, num_cols);
+    assert_eq!(num_rows % 2, 1);
+    assert_eq!(start.0, num_rows / 2);
+    assert_eq!(start.1, num_rows / 2);
+    assert_eq!((num_steps as i32 - start.0) % num_rows, 0);
+
+    let num_cycles = (num_steps as i32 - start.0) / num_rows;
+
     let ds = vec![(1, 0), (-1, 0), (0, 1), (0, -1)];
 
-    if num_steps % 2 != 0 {
-        panic!("not handled");
-    }
+    let mut prev = HashSet::<(i32, i32)>::new();
+    let mut curr = HashSet::<(i32, i32)>::new();
+    curr.insert(start);
 
-    // let mut counts = HashMap::new();
-    // counts.insert(())
-
-    // let mut curr = HashMap::new();
-    // curr.insert(start, 1);
-
-    let mut prev = HashMap::<(i32, i32), u64>::new();
-    let mut curr = HashMap::<(i32, i32), u64>::new();
-    curr.insert(start, 1);
-
-    let mut res = if num_steps % 2 == 0 { 1 } else { 0 };
+    let mut running_count = if num_steps % 2 == 0 { 1 } else { 0 };
+    let mut pts = vec![];
 
     for i in 1..=num_steps {
-        let mut next = HashMap::<(i32, i32), u64>::new();
+        let mut next = HashSet::new();
 
-        for ((r, c), count) in curr.iter() {
+        for (r, c) in curr.iter() {
             for (d_r, d_c) in ds.iter() {
-                let (n_r, n_c) = (
-                    (r + d_r).rem_euclid(num_rows),
-                    (c + d_c).rem_euclid(num_cols),
-                );
-
-                if !rocks.contains(&(n_r, n_c)) && !prev.contains_key(&(n_r, n_c)) {
-                    let prev_count = *next.get(&(n_r, n_c)).unwrap_or(&0);
-                    next.insert((n_r, n_c), prev_count.max(*count));
+                let (n_r, n_c) = (r + d_r, c + d_c);
+                if !rocks.contains(&(n_r.rem_euclid(num_rows), n_c.rem_euclid(num_cols)))
+                    && !prev.contains(&(n_r, n_c))
+                {
+                    next.insert((n_r, n_c));
                 }
             }
         }
 
         if i % 2 == num_steps % 2 {
-            res += next.values().sum::<u64>();
+            running_count += next.len();
+        }
+
+        if i > start.0
+            && (i - start.0) % num_rows == 0
+            && ((i - start.0) / num_rows) % 2 == num_cycles % 2
+        {
+            pts.push(((i / num_rows / 2) as i64, running_count as i64));
+            if pts.len() == 4 {
+                break;
+            }
         }
 
         prev = curr;
         curr = next;
     }
 
-    res
+    let deltas = pts
+        .iter()
+        .zip(pts.iter().skip(1))
+        .map(|(a, b)| b.1 - a.1)
+        .collect::<Vec<_>>();
+    let delta_deltas = deltas
+        .iter()
+        .zip(deltas.iter().skip(1))
+        .map(|(a, b)| b - a)
+        .collect::<Vec<_>>();
+    if delta_deltas[0] != delta_deltas[1] {
+        panic!();
+    }
+
+    let c = delta_deltas[0] / 2;
+    let b = pts[1].1 - pts[0].1 - c * (2 * pts[0].0 + 1);
+    let a = pts[0].1 - b * pts[0].0 - c * pts[0].0 * pts[0].0;
+
+    let x = (num_cycles / 2) as i64;
+    a + b * x + c * x * x
 }
 
 fn parse_input(input: &str) -> ((i32, i32), (i32, i32), HashSet<(i32, i32)>) {
@@ -157,94 +150,20 @@ fn parse_input(input: &str) -> ((i32, i32), (i32, i32), HashSet<(i32, i32)>) {
     ((r_max as i32 + 1, c_max as i32 + 1), start.unwrap(), rock)
 }
 
-fn navigate_big_dump(
-    num_rows: i32,
-    num_cols: i32,
-    start: (i32, i32),
-    rocks: &HashSet<(i32, i32)>,
-    num_steps: usize,
-) -> usize {
-    let ds = vec![(1, 0), (-1, 0), (0, 1), (0, -1)];
-
-    let mut prev = HashSet::<(i32, i32)>::new();
-    let mut curr = HashSet::<(i32, i32)>::new();
-    curr.insert(start);
-
-    let mut res = if num_steps % 2 == 0 { 1 } else { 0 };
-
-    for i in 1..=num_steps {
-        let mut next = HashSet::new();
-
-        for (r, c) in curr.iter() {
-            for (d_r, d_c) in ds.iter() {
-                let (n_r, n_c) = (r + d_r, c + d_c);
-                if !rocks.contains(&(n_r.rem_euclid(num_rows), n_c.rem_euclid(num_cols)))
-                    && !prev.contains(&(n_r, n_c))
-                {
-                    next.insert((n_r, n_c));
-                }
-            }
-        }
-
-        if i % 2 == num_steps % 2 {
-            res += next.len();
-        }
-
-        if i > 65 && (i - 65) % 131 == 0 && ((i - 65) / 131) % 2 == 0 {
-            println!("{}, {}", (i - 65) / 131, res);
-        }
-
-        prev = curr;
-        curr = next;
-    }
-
-    res
-}
-
 fn main() {
-    // println!("Advent of Code 2023");
-    // println!("Day 21: Step Counter");
+    println!("Advent of Code 2023");
+    println!("Day 21: Step Counter");
 
     let now = Instant::now();
 
-    let raw_input = "...........
-.....###.#.
-.###.##..#.
-..#.#...#..
-....#.#....
-.##..S####.
-.##..#...#.
-.......##..
-.##.#.####.
-.##..##.##.
-...........
-";
-
     let raw_input = fs::read_to_string(FILEPATH).expect("Could not read file");
-
     let ((num_rows, num_cols), start, rocks) = parse_input(&raw_input);
 
-    // let part_one = navigate(start, &rocks, 6);
-    let tests = vec![6, 10, 50, 100];
+    let part_one = navigate(start, &rocks, 64);
+    let part_two = navigate_infinite(num_rows, num_cols, start, &rocks, 26501365);
 
-    // for i in tests.into_iter() {
-    //     println!(
-    //         "{} {}",
-    //         i,
-    //         navigate_big_two(num_rows, num_cols, start, &rocks, i)
-    //     );
-    // }
-    // // let test_one = navigate_big_two(num_rows, num_cols, start, &rocks, 50);
-    // println!("{}", test_one);
-    // let part_two = navigate_big_two(num_rows, num_cols, start, &rocks, 26501365);
-    // system.reset();
-    // let part_two = system.find_cycle();
-
-    // println!("Part one: {}", part_one);
-
-    // println!("Part two: {}", part_two);
-
-    navigate_big_dump(num_rows, num_cols, start, &rocks, 26501365);
+    println!("Part one: {}", part_one);
+    println!("Part two: {}", part_two);
 
     println!("Elasped time: {}ms", now.elapsed().as_millis());
 }
